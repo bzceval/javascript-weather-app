ea5b5722ba1c23dee1c875cbe3750417bbd0351e
RynDaniel <ryan@clarusway.com>
2022-03-01 23:23:36 +0300

    folder template commit

--- weather.js
+++ weather.js
@@ -5,28 +5,28 @@ const list = document.querySelector(".ajax-section .cities");
 
 localStorage.setItem("apikey", EncryptStringAES("4d8fb5b93d4af21d66a2948710284366"));
 
-form.addEventListener("submit", e => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   getWeatherDataFromApi();
 });
 
 const getWeatherDataFromApi = async () => {
   let apikey = DecryptStringAES(localStorage.getItem("apikey"));
+  console.log("apikey", apikey);
   let inputVal = input.value;
   let weatherType = "metric";
-  // console.log(apikey);
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apikey}&units=${weatherType}`;
 
   try {
     const response = await axios.get(url);
     // const response = await axios(url);
-
-    console.log(response);
+    console.log("response", response);
     const { main, name, sys, weather } = response.data;
 
     const cityListItems = list.querySelectorAll(".city");
+    console.log("cityListItems", cityListItems);
     const cityListItemArray = Array.from(cityListItems);
-    console.log(cityListItemArray);
+    console.log("cityListItemArray", cityListItemArray);
     if (cityListItemArray.length > 0) {
       const filteredArray = cityListItemArray.filter(card => card.querySelector(".city-name span").innerText == name);
       if (filteredArray.length > 0) {
@@ -38,12 +38,12 @@ const getWeatherDataFromApi = async () => {
     }
     console.log(weather[0].icon);
 
-    const iconUrl = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`
-    console.log(iconUrl);
+    const iconUrl = `https://openweathermap.org/img/wn/${
+      weather[0].icon}@2x.png`;
 
     const createdCityCardLi = document.createElement("li");
     createdCityCardLi.classList.add("city");
-    const createdCityCardLiInnerH = `
+    const createdCityCardLiInnerHTML = `
     <h2 class="city-name" data-name="${name}, ${sys.country}">
         <span>${name}</span>
         <sup>${sys.country}</sup>
@@ -54,7 +54,7 @@ const getWeatherDataFromApi = async () => {
         <figcaption>${weather[0].description}</figcaption>
     </figure>`;
 
-    createdCityCardLi.innerHTML = createdCityCardLiInnerH;
+    createdCityCardLi.innerHTML = createdCityCardLiInnerHTML;
     list.appendChild(createdCityCardLi);
 
     msg.innerText = "";

8815e4af6fb658320414031e7c716c78f5da7aa6
RynDaniel <ryan@clarusway.com>
2022-06-16 19:24:52 +0300

    svgs commit

---
+++ weather.js
@@ -0,0 +1,77 @@
+const form = document.querySelector(".top-banner form");
+const input = document.querySelector(".top-banner input");
+const msg = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".ajax-section .cities");
+
+localStorage.setItem("apikey", EncryptStringAES("4d8fb5b93d4af21d66a2948710284366"));
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getWeatherDataFromApi();
+});
+
+const getWeatherDataFromApi = async () => {
+  let apikey = DecryptStringAES(localStorage.getItem("apikey"));
+  console.log("apikey", apikey);
+  let inputVal = input.value;
+  //Metric and Imperial
+  let units = "metric";
+  //language
+  let lang = "tr";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apikey}&units=${units}&lang=${lang}`;
+
+  try {
+    // const response = await fetch(url);
+    const response = await axios(url);
+    console.log("response", response);
+    const { main, name, sys, weather } = response.data;
+
+    const cityListItems = list.querySelectorAll(".city");
+    console.log("cityListItems", cityListItems);
+    const cityListItemsArray = Array.from(cityListItems);
+    console.log("cityListItemsArray", cityListItemsArray);
+    if (cityListItemsArray.length > 0) {
+      const filteredArray = cityListItemsArray.filter(card => card.querySelector(".city-name span").innerText == name);
+      if (filteredArray.length > 0) {
+        msg.innerText = `You already know the weather for ${name}, Please search for another city 😉`;
+        form.reset();
+        input.focus();
+        return;
+      }
+    }
+    console.log(weather[0].icon);
+
+    ///////////////
+    const iconUrl = `https://openweathermap.org/img/wn/${
+      weather[0].icon}@2x.png`;
+
+    const createdCityCardLi = document.createElement("li");
+    createdCityCardLi.classList.add("city");
+    const createdCityCardLiInnerHTML = `
+    <h2 class="city-name" data-name="${name}, ${sys.country}">
+        <span>${name}</span>
+        <sup>${sys.country}</sup>
+    </h2>
+    <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+    <figure>
+        <img class="city-icon" src="${iconUrl}">
+        <figcaption>${weather[0].description}</figcaption>
+    </figure>`;
+
+    createdCityCardLi.innerHTML = createdCityCardLiInnerHTML;
+    //append vs. prepend
+    list.append(createdCityCardLi);
+
+    msg.innerText = "";
+    // form.reset() ==> input.value = "";
+    form.reset();
+    input.focus();
+  }
+  catch (error) {
+    msg.innerText = error;
+  }
+}
+
+
+
+

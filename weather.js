3fdb15365b08e71d747135d89800f0229e2d9889
bzceval <busraaceval@gmail.com>
2022-09-28 22:21:43 +0300

    try catch function added

--- weather.js
+++ weather.js
@@ -37,6 +37,54 @@ const getWeatherDataFromApi = async () => {
   const lang = "tr";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${tokenKey}&units=${units}&lang=${lang}`;
 
+  try{
+
   const response = await fetch(url).then((response) => response.json());
   console.log(response);
-};
+
+  const { main, sys, weather, name } = response;
+
+  const iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+  constUrlAWS = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
+
+  const cityNameSpans = list.querySelectorAll("city span");
+  const cityNameSpansArray = Array.from(cityNameSpans);
+  if (cityNameSpansArray.length > 0) {
+    const filteredArray = cityNameSpansArray.filter(
+      (span) => span.innerText == name
+    );
+    if(filteredArray.length > 0) {
+        msg.innerText = `You already know the weather for ${name}, Please search for another city :wink:`;
+        setTimeout(()=>{
+            msg.innerText = ""
+        }, 5000)
+        return;
+    }
+  }
+
+  const createdLi = document.createElement("li");
+  //classList add kullandık çünkü diper classları ezmemek için
+  createdLi.classList.add("city");
+  createdLi.innerHTML = `<h2 class="city-name" data-name="${name}, ${
+    sys.country
+  }"> <span>${name}</span> <sup>${sys.country}</sup> </h2>
+    <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+    <figure>
+        <img class="city-icon" src="${iconUrl}">
+        <figcaption>${weather[0].description}</figcaption>
+    </figure>`;
+  //figcaption ve figure elementleri seoyu ön plana çıkarır. Anahtar kelimeler sağlar. önemlissss
+  //append vs prepend
+  //append: bununla arkasına element eklerim
+  //prepend: bununla önüne element eklerim
+  // list.append(createdLi)
+  list.prepend(createdLi); 
+}
+catch (error) {
+    msg.innerText = error;
+        setTimeout(()=>{
+            msg.innerText = ""
+        }, 5000)  
+}
+form.reset()
+}
